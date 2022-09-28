b9cfe0e92c75c4678e3539e908491eea69de9677
meric-coder <meric.kabo@gmail.com>
2022-09-29 00:49:45 +0300

    task1

--- homeWork_2/myCode.js
+++ homeWork_2/myCode.js
@@ -1,76 +1,27 @@
+/*let x = 1;
+let y = 2;
 
-//Task 3 a,b,c
-/* let firstName = "Myroslav";
-let lastName = "Kabo";
-console.log(firstName + " " + lastName); */ 
+let res1 = String(x) + String(y);
+console.log(res1);
+console.log(typeof res1);*/
 
-//Task 3 d,e
-/* let firstName = "Myroslav";
-let lastName = "Kabo";
- console.log(firstName = lastName , lastName); */
+/*let x =1;
+let y = 2;
 
- // Task 4
- /* const newObject = {
-       myString : "Hello World",
-       myNumber : 123,
-       myBoolean : true,
-       myNull :   null,
-       myUndefined : undefined
- };
-    console.log(newObject); */
+let res2 = Boolean(x) + String(y);
+console.log(res2);
+console.log(typeof res2);*/
 
-    //Task 5
-   /* let isAdult = confirm ("Are you adult?");
-    console.log(isAdult); */
+/*let x = 1;
+let y = 2;
 
-    //Task 7
-    /*let myPrompt = prompt("Type the result of 5+5", "Mike");
-    let myPrompt_1 = myPrompt;
-    let myPrompt_2= prompt("This is incorrect", "Mike");*/
+let res3 = Boolean(y - x);
+console.log(res3);
+console.log(typeof res3);*/
 
-    //Task 6
-    /*let myFirstName = "Myroslav ";
-    let myLastName = "Kabo";
-    let myGroup = "Test Automation";
-    let myBirthday = 1994;
-    let isMarried = false;
+/*let x = 1;
+let y = 2;
 
-    console.log( myBirthday, isMarried, myFirstName + myLastName, myGroup);
-
-    let myNull = null;
-    let myUndefined = undefined;
-    console.log(typeof myNull , typeof myUndefined); */
-
-    // Task 7
-   /* let login = prompt("What is your login?"); 
-    let email = prompt("What is your email?");
-    let pass = prompt("What is your password?");
-    alert("Dear " + login + "," + " your email is" + email + ", your pass is" + pass); */
-
-    // Task 8
-    /*const amount = 60;
-    let secondsInHour = prompt("Enter hours");
-    let numSecondsInHour = Number(secondsInHour)*60;
-    alert(" This is how many seconds in " + secondsInHour + "hour(s)" + "=" + amount*numSecondsInHour);
-    let secondsInDay = prompt("Enter days");
-    let numSecondsinDay = Number(secondsInDay)*60;
-    alert("This is how many seconds in " + secondsInDay + "day(s)" + "=" + amount*numSecondsinDay*24);
-
-    let amountOfMonths = prompt("Enter amount of months");
-    let numDaysInMonth = Number(amountOfMonths);
-
-    let amountOfDays = prompt("Enter amount of days in Months");
-    let numAmountOfDays = Number(amountOfDays);
-    let secondsInMonth = numDaysInMonth*numAmountOfDays*amount*24*60;
-
-    alert("This is how many seconds in " + numDaysInMonth + " months =" + secondsInMonth);*/
-
-    
-    
-
-    
-
-    
-
-
-    
+let res4 = (Boolean(x) + String(y))*y;
+console.log(res4);
+console.log(typeof res4);*/
