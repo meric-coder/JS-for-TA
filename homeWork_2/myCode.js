45ee298d8929799c88f1228a424e7ba9617ffbe9
meric-coder <meric.kabo@gmail.com>
2022-09-28 23:11:25 +0300

    new changes

---
+++ homeWork_2/myCode.js
@@ -0,0 +1,76 @@
+
+//Task 3 a,b,c
+/* let firstName = "Myroslav";
+let lastName = "Kabo";
+console.log(firstName + " " + lastName); */ 
+
+//Task 3 d,e
+/* let firstName = "Myroslav";
+let lastName = "Kabo";
+ console.log(firstName = lastName , lastName); */
+
+ // Task 4
+ /* const newObject = {
+       myString : "Hello World",
+       myNumber : 123,
+       myBoolean : true,
+       myNull :   null,
+       myUndefined : undefined
+ };
+    console.log(newObject); */
+
+    //Task 5
+   /* let isAdult = confirm ("Are you adult?");
+    console.log(isAdult); */
+
+    //Task 7
+    /*let myPrompt = prompt("Type the result of 5+5", "Mike");
+    let myPrompt_1 = myPrompt;
+    let myPrompt_2= prompt("This is incorrect", "Mike");*/
+
+    //Task 6
+    /*let myFirstName = "Myroslav ";
+    let myLastName = "Kabo";
+    let myGroup = "Test Automation";
+    let myBirthday = 1994;
+    let isMarried = false;
+
+    console.log( myBirthday, isMarried, myFirstName + myLastName, myGroup);
+
+    let myNull = null;
+    let myUndefined = undefined;
+    console.log(typeof myNull , typeof myUndefined); */
+
+    // Task 7
+   /* let login = prompt("What is your login?"); 
+    let email = prompt("What is your email?");
+    let pass = prompt("What is your password?");
+    alert("Dear " + login + "," + " your email is" + email + ", your pass is" + pass); */
+
+    // Task 8
+    /*const amount = 60;
+    let secondsInHour = prompt("Enter hours");
+    let numSecondsInHour = Number(secondsInHour)*60;
+    alert(" This is how many seconds in " + secondsInHour + "hour(s)" + "=" + amount*numSecondsInHour);
+    let secondsInDay = prompt("Enter days");
+    let numSecondsinDay = Number(secondsInDay)*60;
+    alert("This is how many seconds in " + secondsInDay + "day(s)" + "=" + amount*numSecondsinDay*24);
+
+    let amountOfMonths = prompt("Enter amount of months");
+    let numDaysInMonth = Number(amountOfMonths);
+
+    let amountOfDays = prompt("Enter amount of days in Months");
+    let numAmountOfDays = Number(amountOfDays);
+    let secondsInMonth = numDaysInMonth*numAmountOfDays*amount*24*60;
+
+    alert("This is how many seconds in " + numDaysInMonth + " months =" + secondsInMonth);*/
+
+    
+    
+
+    
+
+    
+
+
+    
