f152153fcdfefdb1de768b2e33343fe69bbad58b
meric-coder <meric.kabo@gmail.com>
2022-09-29 11:20:31 +0300

    task 3

--- homeWork_2/myCode.js
+++ homeWork_2/myCode.js
@@ -36,4 +36,22 @@ let divider = 7;
 let res = (age > 0 && age % divider == 0) ? "Yes" : "No";
 console.log(res);*/
 
+//Task 3
+/*const arr = [];
+arr[0] = 1;
+arr[1] = "Hello";
+arr[2] = Boolean(arr[0]);
+arr[3] = null;
+
+alert(arr.length);
+
+let entered = prompt("Please enter any digit");
+arr.push(entered);
+console.log(arr[4]);
+
+arr.splice(0,1);
+console.log(arr);*/
+
+
+
 
