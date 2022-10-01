ab3806446d83dc759c529b6603358f9a88fb9358
meric-coder <meric.kabo@gmail.com>
2022-10-01 11:23:13 +0300

    homeWork 3

--- homeWork_3/myCode.js
+++ homeWork_3/myCode.js
@@ -1,2 +1,30 @@
-let a = 444;
-console.log(a);
+// Task 1
+
+/*let arr = [2,3,4,5];
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+console.log(sum);*/
+
+// Task 1 (While)
+
+/*let arr = [2,3,4,5];
+let i = 0;
+let sum = 0;
+while ( arr[i]) {
+    sum += arr[i];
+    i++;
+}
+console.log(sum);*/
+
+// Task 2
+
+/*let arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+        console.log(arr[i] + " is even");
+    } else if (arr[i] % 2 !== 0) {
+        console.log(arr[i] + " is odd");
+    }
+}*/
