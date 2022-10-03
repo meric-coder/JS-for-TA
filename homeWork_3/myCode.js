d8e20760a3461b81deae6c41fb32ae107a4ae3c3
meric-coder <meric.kabo@gmail.com>
2022-10-03 21:32:24 +0300

    homeWork 3

--- homeWork_3/myCode.js
+++ homeWork_3/myCode.js
@@ -28,3 +28,56 @@ for (let i = 0; i < arr.length; i++) {
         console.log(arr[i] + " is odd");
     }
 }*/
+
+
+// Task 3 (a)
+
+/*function randArray(k){
+    let arr = [];
+    for (i = 0; i < k; i++) {
+    arr.push(Math.round(Math.random()*1000/2));
+}
+return arr;
+}
+
+console.log(randArray(5));*/
+
+// Task 3 (b)
+
+/*function randomArray(k) {
+    return Array.apply(null, Array(k)).map(function() {
+        return Math.round(Math.random() *1000/2);
+    });
+}
+console.log(randomArray(5));*/
+
+// Task 4
+
+/*function raiseToDegree(a,b) {
+    if (isNaN(a) || isNaN(b)) {
+        console.log("Please enter digits");
+    } else {
+        return a**b;
+        }
+}
+let a = prompt("Please enter digit - a");
+let b = prompt("Please enter digit - b");
+
+console.log(raiseToDegree(Number(a),Number(b)));*/
+
+// Task 5
+ /*function findMin(){
+    let min = 0;
+    for (let i = 0; i < arguments.length; i++) {
+    if (min > arguments[i]) {
+        min = arguments[i];
+        console.log(min);
+    } 
+ };
+ }
+findMin(12,14,4,-4,0.2);
+
+
+
+
+   
