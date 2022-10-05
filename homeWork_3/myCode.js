c1a4d135712861fc6481e179c09bf09e5ba1d5e9
meric-coder <meric.kabo@gmail.com>
2022-10-05 21:25:12 +0300

    homeWork 3

--- homeWork_3/myCode.js
+++ homeWork_3/myCode.js
@@ -1,38 +1,38 @@
-// Task 1
+// Task 1 (for)
 
-/*let arr = [2,3,4,5];
+let arr = [2,3,4,5];
 let sum = 0;
 for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
 }
-console.log(sum);*/
+console.log(sum);
 
 // Task 1 (While)
 
-/*let arr = [2,3,4,5];
+let arr = [2,3,4,5];
 let i = 0;
 let sum = 0;
 while ( arr[i]) {
     sum += arr[i];
     i++;
 }
-console.log(sum);*/
+console.log(sum);
 
 // Task 2
 
-/*let arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 for (let i = 0; i < arr.length; i++) {
     if (arr[i] % 2 == 0) {
         console.log(arr[i] + " is even");
     } else if (arr[i] % 2 !== 0) {
         console.log(arr[i] + " is odd");
     }
-}*/
+}
 
 
 // Task 3 (a)
 
-/*function randArray(k){
+function randArray(k){
     let arr = [];
     for (i = 0; i < k; i++) {
     arr.push(Math.round(Math.random()*1000/2));
@@ -40,20 +40,20 @@ for (let i = 0; i < arr.length; i++) {
 return arr;
 }
 
-console.log(randArray(5));*/
+console.log(randArray(5));
 
 // Task 3 (b)
 
-/*function randomArray(k) {
+function randomArray(k) {
     return Array.apply(null, Array(k)).map(function() {
         return Math.round(Math.random() *1000/2);
     });
 }
-console.log(randomArray(5));*/
+console.log(randomArray(5));
 
 // Task 4
 
-/*function raiseToDegree(a,b) {
+function raiseToDegree(a,b) {
     if (isNaN(a) || isNaN(b)) {
         console.log("Please enter digits");
     } else {
@@ -63,10 +63,10 @@ console.log(randomArray(5));*/
 let a = prompt("Please enter digit - a");
 let b = prompt("Please enter digit - b");
 
-console.log(raiseToDegree(Number(a),Number(b)));*/
+console.log(raiseToDegree(Number(a),Number(b)));
 
 // Task 5
- /*function findMin(){
+ function findMin(){
     let min = 0;
     for (let i = 0; i < arguments.length; i++) {
     if (min > arguments[i]) {
@@ -75,11 +75,11 @@ console.log(raiseToDegree(Number(a),Number(b)));*/
     } 
  };
  }
-findMin(12,14,4,-4,0.2);*/
+findMin(12,14,4,-4,0.2);
 
 // Task 6
 
-/*function findUnique(arr) {
+function findUnique(arr) {
     for (i = 0; i < arr.length; i++) {
         for (let k = (i + 1); k < arr.length; k++) {
             if (arr[k] === arr[i]) {
@@ -90,31 +90,30 @@ findMin(12,14,4,-4,0.2);*/
     return true;
 }
 
-console.log(findUnique([1,3,3,4,4]));*/
+console.log(findUnique([1,3,3,4,4]));
 
 // Task 7
 
-/*function lastElem(arr,count) {
+function lastElem(arr,count) {
     return (count == null) ? arr.pop() : arr.slice(-count);
 }
 
 
 console.log( lastElem([3,4,10,-5]));
 console.log(lastElem([3,4,10,-5], 2 ));
-console.log(lastElem([3,4,10,-5], 8));*/
+console.log(lastElem([3,4,10,-5], 8));
 
 // Task 8
-function firstLetter(str) {
-    const arr1 = str.split(' ');
-    return arr1;
+function bigFirstLetter(str) {
+    let arr1 = str.split(' ');
+    let arr2 = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        arr2.push(arr1[i].charAt(0).toUpperCase() + arr1[i].slice(1));
+    
+}
+return arr2.join(' ');
 }
 
+console.log(bigFirstLetter("i love java script"));
 
-
-console.log(firstLetter("i love java script"));
-
-
-
-
-
-   
