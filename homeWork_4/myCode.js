9f380bfab858999d5d5e4e0b50c436d74eccc3e5
meric-coder <meric.kabo@gmail.com>
2022-10-05 21:59:21 +0300

    homeWork 4

---
+++ homeWork_4/myCode.js
@@ -0,0 +1,23 @@
+// Task 1
+/*try {
+function calcRectangleArea(width , height) {
+    if (isNaN(width) || isNaN(height) || width < 0 || height < 0 ) {
+    throw new Error("String entered or negative value");
+    } else if  (width * height <=0) {
+        throw new Error("Width or Height is not entered");
+    } else  {
+        console.log(width * height);;
+    }
+
+}
+
+let a = prompt("Enter value a");
+let b = prompt("Enter value b");
+
+calcRectangleArea(a,b);
+
+} catch (exception) {
+    console.log(exception.stack);
+}*/
+
+
