333e89b518dd39b6c413a883e77dbc37565ac78b
meric-coder <meric.kabo@gmail.com>
2022-10-09 09:23:52 +0300

    homeWork 4

--- homeWork_4/myCode.js
+++ homeWork_4/myCode.js
@@ -1,5 +1,5 @@
 // Task 1
-/*try {
+try {
 function calcRectangleArea(width , height) {
     if (isNaN(width) || isNaN(height) || width < 0 || height < 0 ) {
     throw new Error("String entered or negative value");
@@ -18,6 +18,81 @@ calcRectangleArea(a,b);
 
 } catch (exception) {
     console.log(exception.stack);
-}*/
+}
 
 
+// Task 2
+try {
+function checkAge(userAge) {
+    if (isNaN(userAge)){
+        alert("String is entered.Please enter a number");
+        throw new Error("String entered.Please enter a number");
+    } else if (userAge == null) {
+        alert("The field is empty.Please enter your age");
+        throw new Error("The field is empty.Please enter your age");
+    } else if (userAge < 14) {
+     alert("User's age < 14 years");
+     throw new Error("User's age < 14 years");
+    } else {
+        alert("You can watch the movie at https://megogo.net/ua");
+    }
+
+}
+
+let enterAge = prompt("Enter age");
+
+checkAge(Number(enterAge));
+}
+catch  (exception) {
+    console.log(exception.stack);
+}
+
+// Task 3
+
+class MonthException {
+    constructor(message) {
+        this.message = message;
+        this.month = "MonthException";
+    }
+}
+
+function showMonthName(month) {
+     month = month - 1;
+    let months = ["JAN", "FEB","MAR", "APR", "MAY", "JUN","JUL", "AUG","SEP", "OCT", "NOV", "DEC"];
+    if (months[month] !==undefined) {
+        return months[month];
+    }else {
+        throw new MonthException(month + " is not a valid month");
+    }
+}
+
+try {
+    let myMonth = prompt("Please enter a valid month");
+    let monthName = showMonthName(myMonth);
+    console.log(monthName);
+} catch (e) {
+    console.log(e.message);
+}
+
+// Task 4
+
+function showUser(id) {
+    if (id < 0) {
+        throw new Error (id + " is a negative value");
+    }
+    return {id };
+}
+
+function showUsers(ids) {
+    let result = [];
+    ids.forEach(function (id) {
+        try {
+            let person = showUser(id);
+            result.push(person);
+        } catch (e) {
+            console.log(e.message);
+        }
+    });
+}
+    showUsers ([7, -12, 44, 22]);
+    
