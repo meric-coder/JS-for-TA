3a81b641928ed58bf614fcf39639a5a78459209b
meric-coder <meric.kabo@gmail.com>
2022-10-12 21:54:46 +0300

    homeWork 5

--- homeWork_5/myCode.js
+++ homeWork_5/myCode.js
@@ -14,7 +14,7 @@ console.log(propsCount(mentor));*/
 
 // Task 2
 
-let newObj = {
+/*let newObj = {
     name: "Lincoln",
     surname: "Abraham",
     age: 75,
@@ -28,4 +28,63 @@ function showProps(obj) {
     console.log(Object.values(obj));
 }
 
-showProps(newObj);
+showProps(newObj);*/
+
+// Task 3
+
+/*class Person {
+    constructor(name,surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    showFullName(name,surname) {
+        return this.name + " " + this.surname;
+    }
+
+}
+
+class Student extends Person {
+    constructor(name, surname, year) {
+        super(name, surname);
+        this.year = year;
+    
+}
+  
+showFullName(middleName) {
+    return this.name + " " + middleName + " " + this.surname;
+}
+
+ showCourse() {
+    return new Date().getFullYear() - this.year;
+  }
+
+}
+  const stud1 = new Student("Max", "Petrovych", 2015);
+  console.log(stud1.showFullName("Olegovych"));
+  console.log("Current course: " + stud1.showCourse());
+  */
+
+
+  // Task 4
+
+  class Worker {
+    _experience = 2;
+  constructor(fullName, dayRate, workingDays)  {
+    this.fullName = fullName;
+    this.dayRate = dayRate;
+    this.workingDays = workingDays;
+     
+  }
+    showSalary() {
+        return this.dayRate * this.workingDays;
+    }
+
+   showSalaryWithExperience() {
+    return this.dayRate * this.workingDays * this._experience;
+   }
+  }
+
+let rab = new Worker("Oleg Bohdanovych", 2, 100);
+console.log(rab.showSalaryWithExperience());
+
+
