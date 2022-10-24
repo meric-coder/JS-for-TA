10d682dad0391bfb934e2c4e42089668f618df69
meric-coder <meric.kabo@gmail.com>
2022-10-24 21:30:43 +0300

    homeWork 7

---
+++ homeWork_7/myCode.js
@@ -0,0 +1,79 @@
+
+//Task 1
+
+/*let myWindow = window.open("https://www.w3schools.com/jsref/met_win_settimeout.asp" , "Hello World" , "width = 300, height = 300");
+
+setTimeout(function(){
+    myWindow.resizeTo(500,500);
+} , 3000);
+
+setTimeout(function(){
+    myWindow.moveTo(200,200);
+} , 3000);
+
+setTimeout(function(){
+    myWindow.close();
+} , 6000);*/
+
+// Task 2
+
+/*
+   function myFunction () {
+    let myElement = document.getElementById("text");
+
+    myElement.style.color = "orange";
+    myElement.style.fontSize = "20pt";
+    myElement.style.fontFamily = "Comic Sans MS";
+    
+   }*/
+
+   // Task 3
+
+   /*function setBackground(color) {
+
+    document.body.style.background = color;
+   }*/
+
+
+   // Task 4
+/*
+   myBtn.addEventListener("click",deleteColor);
+
+   function deleteColor() {
+    let myList = document.getElementById("dropValues");
+    myList.remove(myList.selectedIndex);
+   }
+   
+   deleteColor();
+*/
+
+// Task 5
+/*
+         myBtn.addEventListener("mouseover",addMouseOver);
+         myBtn.addEventListener("click",addEventClick);
+         myBtn.addEventListener("mouseout",mouseOut);
+         function addMouseOver() {
+              
+          let myElement =  document.getElementById("myBtn").innerHTML;
+          console.log("Mouse Over");
+         }
+         function addEventClick() {
+            let myElement = document.getElementById("myBtn").innerHTML;
+            console.log("I was clicked");
+         }
+         function mouseOut() {
+          let myElement = document.getElementById("myBtn").innerHTML;
+          console.log("Mouse is not on me");
+
+         }*/
+
+
+
+   
+
+
+
+   
+    
+
+
